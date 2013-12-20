1d0ebe287df5f85210f01b477e286fd66383c6aa
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-12-20 17:15:03 +0100

    Added ClickTopology. Added Conf classes

---
+++ src/main/java/storm/cookbook/Conf.java
@@ -0,0 +1,9 @@
+package storm.cookbook;
+
+/**
+ * User: domenicosolazzo
+ */
+public class Conf {
+    public static final String REDIS_HOST_KEY = "redisHost";
+    public static final String REDIS_PORT_KEY = "redisPort";
+}
